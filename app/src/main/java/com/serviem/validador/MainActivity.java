1b11a2ced29d44cf5f9b6981e7f3b72c8f2c1759
frojasrv-dot <frojasrv@gmail.com>
2026-04-26 17:12:25 -0500

    Update MainActivity.java

--- app/src/main/java/com/serviem/validador/MainActivity.java
+++ app/src/main/java/com/serviem/validador/MainActivity.java
@@ -1,15 +1,19 @@
 package com.serviem.validador;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.provider.Settings;
 
 public class MainActivity extends Activity {
     @Override
-    protected void Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
-        tv.setText("SERVICIO VALIDADOR ACTIVO\nID: " + 
-            android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID));
+        String androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        tv.setText("SERVICIO VALIDADOR ACTIVO\nTU ID ES: " + androidId);
+        tv.setTextSize(20);
+        tv.setPadding(50, 50, 50, 50);
         setContentView(tv);
     }
 }
